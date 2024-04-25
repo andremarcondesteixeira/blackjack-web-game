aab7c80068e12e67c1f166f59f775a0f93336878
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-25 19:08:38 -0300

    A new Game object can be created

--- src/domain/game.js
+++ src/domain/game.js
@@ -1,30 +1,17 @@
-import { Lobby_State } from "./lobby_state.js";
-
 export class Game {
   #players;
   #amount_of_decks;
-  #state;
 
-  constructor() {
-    this.#players = new Array(7).fill(null);
-    this.#amount_of_decks = 1;
-    this.#state = new Lobby_State({
-      add_player_to_seat: (player, seat_number) => this.#players[seat_number - 1] = player,
-      remove_player_on_seat: seat_number => this.#players[seat_number - 1] = null,
-      set_amount_of_decks: amount => this.#amount_of_decks = amount,
-    });
-    Object.seal(this.#state);
+  constructor({ players, amount_of_decks }) {
+    this.#players = Object.freeze(players);
+    this.#amount_of_decks = amount_of_decks;
   }
 
-  get amount_of_players() {
-    return this.#players.filter(p => !!p).length;
+  get players() {
+    return this.#players;
   }
 
   get amount_of_decks() {
     return this.#amount_of_decks;
-  }
-
-  get state() {
-    return this.#state;
   }
 }
