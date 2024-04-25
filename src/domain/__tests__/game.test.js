835aa0528d4b4a6c5707ae69dfe2c567bc226179
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-25 02:10:36 -0300

    Failing test for new Game constructor

--- src/domain/__tests__/game.test.js
+++ src/domain/__tests__/game.test.js
@@ -5,29 +5,12 @@ import { make_player } from "./helpers.js";
 
 suite("Game", () => {
   test("A Game object can be created", () => {
-    const game = new Game({
-      players: [
-        make_player({
-          name: "Mary",
-          balance: 1000
-        }),
-        make_player({
-          name: "John",
-          balance: 1500
-        })
-      ],
-      amount_of_decks: 3
-    });
-
-    assert.equal(game.players.length, 2);
-    assert.equal(game.players[0].name, "Mary");
-    assert.equal(game.players[0].balance, 1000);
-    assert.equal(game.players[1].name, "John");
-    assert.equal(game.players[1].balance, 1500);
-    assert.equal(game.amount_of_decks, 3);
+    const game = new Game();
+    assert.equal(game.amount_of_players, 0);
+    assert.equal(game.amount_of_decks, 1);
   });
 
-  test("At least 1 player must be playing the game", () => {
+  test.skip("At least 1 player must be playing the game", () => {
     for (const players of [null, undefined, [], {}]) {
       assert.throws(() => {
         new Game({ players, amount_of_decks: 1 });
@@ -35,7 +18,7 @@ suite("Game", () => {
     }
   });
 
-  test("A maximum of 7 players can join the game", () => {
+  test.skip("A maximum of 7 players can join the game", () => {
     assert.throws(() => {
       const players = new Array(8).fill(make_player({ name: "Carl", balance: 1200 }));
       new Game({ players });
