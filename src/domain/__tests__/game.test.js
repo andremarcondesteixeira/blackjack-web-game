26db668ae4526dcdddfc9c6e3e7f70fa79a42836
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-24 21:59:34 -0300

    Increments test with how I imagine the Game class being instantiated

--- src/domain/__tests__/game.test.js
+++ src/domain/__tests__/game.test.js
@@ -1,8 +1,28 @@
+import { strict as assert } from "node:assert";
 import { suite, test } from "node:test";
 import { Game } from "../game.js";
 
 suite("Game", () => {
   test("A Game object can be created", () => {
-    const game = new Game();
+    const game = new Game({
+      players: [
+        {
+          name: "Mary",
+          balance: 1000
+        },
+        {
+          name: "John",
+          balance: 1500
+        }
+      ],
+      amount_of_decks: 2
+    });
+
+    assert.equal(game.players.length, 2);
+    assert.equal(game.players[0].name, "Mary");
+    assert.equal(game.players[0].balance, 1000);
+    assert.equal(game.players[1].name, "John");
+    assert.equal(game.players[1].balance, 1500);
+    assert.equal(game.amount_of_decks, 2);
   });
 });
