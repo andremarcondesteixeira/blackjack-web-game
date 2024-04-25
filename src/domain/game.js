1a1eb163cc2c821985b43c79e98823fe3d34a08d
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-25 02:12:41 -0300

    Game's new constructor

--- src/domain/game.js
+++ src/domain/game.js
@@ -2,21 +2,13 @@ export class Game {
   #players;
   #amount_of_decks;
 
-  constructor({ players, amount_of_decks }) {
-    if (!players || !(players.length > 0)) {
-      throw new Error("At least 1 player must play the game");
-    }
-
-    if (players.length > 7) {
-      throw new Error("No more than 7 players are allowed to join");
-    }
-
-    this.#players = players;
-    this.#amount_of_decks = amount_of_decks;
+  constructor() {
+    this.#players = [];
+    this.#amount_of_decks = 1;
   }
 
-  get players() {
-    return this.#players;
+  get amount_of_players() {
+    return this.#players.length;
   }
 
   get amount_of_decks() {
