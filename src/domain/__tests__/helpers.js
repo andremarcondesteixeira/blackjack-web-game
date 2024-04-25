056e914d5c888fb0f51008f63092c64564b1b334
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-24 23:42:23 -0300

    Test refactor: Creates "garbage" helper to test parameter validation

--- src/domain/__tests__/helpers.js
+++ src/domain/__tests__/helpers.js
@@ -1,3 +1,45 @@
+// only meant to use when testing parameter validation, otherwise it would cost a significant overhead during test runs
+export const garbage = [
+  null,
+  undefined,
+  NaN,
+  Infinity,
+  true,
+  false,
+  () => { },
+  function () { },
+  function foo() { },
+  class {},
+  class Bar {},
+  Date.now(),
+  {},
+  [],
+  "",
+  "    ",
+  -998,
+  -9998.009389
+  +0,
+  -0,
+  0,
+  0.000001,
+  87288.9987,
+  Number.MAX_VALUE,
+  Number.MIN_VALUE,
+  Number.NaN,
+  Number.MAX_SAFE_INTEGER,
+  Number.MIN_SAFE_INTEGER,
+  Number.EPSILON,
+  BigInt("99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"),
+];
+
+export function serialize_garbage(garbage) {
+  if (typeof garbage === "bigint") {
+    return garbage.toString();
+  }
+
+  return JSON.stringify(garbage);
+}
+
 export function make_player({ name, balance }) {
   return { name, balance };
 }
--- src/domain/__tests__/player.test.js
+++ src/domain/__tests__/player.test.js
@@ -1,6 +1,7 @@
 import { Player } from "../player.js";
 import { strict as assert } from "node:assert";
 import { suite, test } from "node:test";
+import { garbage, serialize_garbage } from "./helpers.js";
 
 suite("Player", () => {
   test("A new Player object can be created", () => {
@@ -11,10 +12,10 @@ suite("Player", () => {
 
   suite("Illegal states are unrepresentable", () => {
     test("A player must have a name", () => {
-      for (const name of [null, undefined, "", "    ", {}, [], Date.now(), () => {}, 0, NaN]) {
+      for (const name of garbage) {
         assert.throws(() => {
           new Player({ name, balance: 100 });
-        }, `Should throw exception when player name is ${JSON.stringify(name)}`);
+        }, `Should throw exception when player name is ${serialize_garbage(name)}`);
       }
     });
   });
