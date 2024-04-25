e98949aeac3bf49c2af2dbaab785071c37254107
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-25 20:20:28 -0300

    constants for max allowed amount of players and decks

--- src/domain/game.js
+++ src/domain/game.js
@@ -1,6 +1,9 @@
 import { is_an_actual_number, serialize } from "../util.js";
 import { Player } from "./player.js";
 
+const DECKS_MAX_ALLOWED_AMOUNT = 8;
+const PLAYERS_MAX_ALLOWED_AMOUNT = 7;
+
 export class Game {
   #players;
   #amount_of_decks;
@@ -10,8 +13,8 @@ export class Game {
       throw new Error(`Expected an array of players, but got ${serialize(players)} instead`);
     }
 
-    if (players.length > 7) {
-      throw new Error(`No more than 7 player can join the game, but tried to play with ${players.length} players`);
+    if (players.length > PLAYERS_MAX_ALLOWED_AMOUNT) {
+      throw new Error(`No more than ${PLAYERS_MAX_ALLOWED_AMOUNT} player can join the game, but tried to play with ${players.length} players`);
     }
 
     for (let i = 0; i < players.length; i++) {
@@ -24,8 +27,8 @@ export class Game {
       throw new Error(`Amount of decks must be a positive integer greather than zero, but got ${serialize(amount_of_decks)} instead`);
     }
 
-    if (amount_of_decks > 8) {
-      throw new Error(`Maximum allowed amount of decks is 8, but got ${amount_of_decks} instead`);
+    if (amount_of_decks > DECKS_MAX_ALLOWED_AMOUNT) {
+      throw new Error(`Maximum allowed amount of decks is ${DECKS_MAX_ALLOWED_AMOUNT}, but got ${amount_of_decks} instead`);
     }
 
     this.#players = Object.freeze(players);
