2c733c12220f2f05ca1d94648a48daf5c241e80e
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-26 15:41:19 -0300

    Player tests fixed

--- src/domain/__tests__/player.test.js
+++ src/domain/__tests__/player.test.js
@@ -1,7 +1,8 @@
-import { Player } from "../player.js";
 import { strict as assert } from "node:assert";
 import { suite, test } from "node:test";
-import { create_garbage, serialize_garbage } from "./helpers.js";
+import { serialize } from "../../util.js";
+import { Player } from "../player.js";
+import { make_garbage } from "./helpers.js";
 
 suite("Player", () => {
   test("A new Player object can be created", () => {
@@ -12,16 +13,16 @@ suite("Player", () => {
 
   suite("Validation", () => {
     test("A player must have a name", () => {
-      const garbage = create_garbage();
+      const garbage = make_garbage();
       for (const name of garbage) {
         assert.throws(() => {
           new Player({ name, balance: 100 });
-        }, `Should throw exception when player name is ${serialize_garbage(name)}`);
+        }, `Should throw exception when player name is ${serialize(name)}`);
       }
     });
 
     test("A player's balance must be a finite number greater than or equal to 0, or null, or undefined", () => {
-      const garbage = create_garbage({
+      const garbage = make_garbage({
         do_not_include_positive_numbers_except_zero: true,
         do_not_include_zero: true,
         do_not_include_null: true,
@@ -30,7 +31,7 @@ suite("Player", () => {
       for (const balance of garbage) {
         assert.throws(() => {
           new Player({ name: "Claudia", balance });
-        }, `Should throw exception when player's balance is ${serialize_garbage(balance)}`);
+        }, `Should throw exception when player's balance is ${serialize(balance)}`);
       }
     });
   });
