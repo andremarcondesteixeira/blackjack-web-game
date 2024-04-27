74c02f97145eb5fffc14d6b2678365095b47749a
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-27 14:15:51 -0300

    name_of refactoring

--- src/util.js
+++ src/util.js
@@ -2,12 +2,21 @@ export function is_an_actual_number(x) {
   return typeof x === "number" && !Number.isNaN(x) && Number.isFinite(x);
 }
 
+/**
+ * Usage: pass in a function, or object, like:
+ *
+ * name_of(my_function);
+ * name_of({ my_variable }); // enclosed by curly brackets
+ *
+ * @param {Function | object} x
+ * @returns the name of the variable, function, object or class
+ */
 export function name_of(x) {
-  if (typeof x === 'function') {
-    return x.name ?? x.constructor.name;
+  if (!['function', 'object'].includes(typeof x) || x instanceof Array) {
+    throw new Error(`name_of expects to get a function or object. Got ${serialize(x)} instead.`);
   }
 
-  return Object.keys({ x })[0];
+  return x.name ?? x.constructor.name ?? Object.keys(x)[0];
 }
 
 export function serialize(x) {
