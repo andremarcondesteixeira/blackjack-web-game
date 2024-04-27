637c428e5f25e52e5de98c11f47abe5779f6cac0
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-27 14:18:00 -0300

    A new Players_Betting_State object can be created

---
+++ src/domain/game_states/__tests__/players_betting_state.test.js
@@ -0,0 +1,22 @@
+import { strict as assert } from "node:assert";
+import { suite, test } from "node:test";
+import { Players_Betting_State } from "../players_betting_state.js";
+import { name_of } from "../../../util.js";
+import { make_player } from "../../../test_helpers.js";
+import { make_shuffled_decks } from "../../card.js";
+
+suite(name_of(Players_Betting_State), () => {
+  suite("Happy path", () => {
+    test(`A new ${name_of(Players_Betting_State)} object can be created`, () => {
+      const player = make_player();
+      const state = new Players_Betting_State({
+        players: [player],
+        cards: make_shuffled_decks(1)
+      });
+
+      assert.equal(state.players[0].name, player.name);
+      assert.equal(state.players[0].balance, player.balance);
+      assert.equal(state.cards.length, 52);
+    });
+  });
+});
--- src/domain/game_states/players_betting_state.js
+++ src/domain/game_states/players_betting_state.js
@@ -1,3 +1,17 @@
 export class Players_Betting_State {
+  #cards;
+  #players;
 
+  constructor({ cards, players }) {
+    this.#cards = Object.freeze(cards);
+    this.#players = Object.freeze(players);
+  }
+
+  get cards() {
+    return this.#cards;
+  }
+
+  get players() {
+    return this.#players;
+  }
 }
