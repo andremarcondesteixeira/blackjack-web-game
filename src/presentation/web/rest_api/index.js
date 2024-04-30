6d5bc7f88695c946fc115854c322c3e94bcc54ad
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-04-30 08:27:28 -0300

    uses koa router to define root route

--- src/presentation/web/rest_api/index.js
+++ src/presentation/web/rest_api/index.js
@@ -1,9 +1,15 @@
 import Koa from "koa";
+import Router from "@koa/router";
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async context => {
+router.get("/", context => {
   context.body = "It works!";
-});
+})
 
-app.listen(3000);
+app.use(router.routes())
+   .use(router.allowedMethods())
+   .listen(3000, () => {
+    console.log("server is listening on port 3000");
+   });
